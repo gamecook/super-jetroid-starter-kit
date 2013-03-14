f3e9e9d25e92c314ea2604d31c3a1b74df63ad35
Jesse Freeman <jessefreeman@gmail.com>
2013-03-14 19:15:32 -0400

    intial import

---
+++ js/resize-game.js
@@ -0,0 +1,26 @@
+﻿window.addEventListener('resize', resizeGame, false);
+
+function resizeGame() {
+
+    var gameCanvas = document.getElementById('canvas');
+            
+    var widthToHeight = gameCanvas.width / gameCanvas.height;
+    var newWidth = window.innerWidth;
+    var newHeight = window.innerHeight;
+    var newWidthToHeight = newWidth / newHeight;
+
+    if (newWidthToHeight > widthToHeight) {
+        newWidth = newHeight * widthToHeight;
+        gameCanvas.style.height = newHeight + 'px';
+        gameCanvas.style.width = newWidth + 'px';
+    } else {
+        newHeight = newWidth / widthToHeight;
+        gameCanvas.style.width = newWidth + 'px';
+        gameCanvas.style.height = newHeight + 'px';
+    }
+
+    gameCanvas.style.marginTop = (-newHeight / 2) + 'px';
+    gameCanvas.style.marginLeft = (-newWidth / 2) + 'px';
+
+    
+}
